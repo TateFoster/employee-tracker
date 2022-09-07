05814e89662db296830a577fed3a1cc51381c13f
Dragon Lore <tate.j.foster@gmail.com>
2022-09-06 19:15:38 -0600

    got schema.sql working to fully set up, got seeds.sql to populate tables with example roles, departments and employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,29 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
+DROP DATABASE IF EXISTS employeetracker_db;
 
-CREATE DATABASE employeeTracker_db;
+CREATE DATABASE employeetracker_db;
 
-USE DATABASE employeeTracker_db;
+USE employeetracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(40)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30)
-    role_id INT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE CASCADE,
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES 
+("Legal"),
+("Sales"),
+("Human Resources");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+("Lawyer", 120000, 1),
+("Sales Manager", 90000, 2),
+("Head of Human Resources", 150000, 3),
+("Salesperson", 45000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Richard", "Williams", 1, null),
+("Linda", "Wright", 2, null),
+("Jeremy", "Boob", 4, 2),
+("Diana", "Bobana", 3, null);
