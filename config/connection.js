c805fa771a83b893408ef94f6d8c1d560a5a575b
Dragon Lore <tate.j.foster@gmail.com>
2022-09-08 18:22:08 -0600

    put in .env file for security

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
 	host: "localhost",
-	user: "root",
-	password: "wigglebouncecodedown",
-	database: "employeetracker_db",
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
 module.exports = connection;
